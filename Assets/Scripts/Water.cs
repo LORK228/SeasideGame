1dc845bafb284e8c3f4025ba60ba3ce5a660a1d5
LORK228 <85165063+LORK228@users.noreply.github.com>
2022-06-12 13:50:33 +0700

    оаоп

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    private Material[] m_arr_material;
+    public float speed;
+    private float timer;
+    void Start()
+    {
+        timer = 0;
+        m_arr_material = GetComponent<Renderer>().materials;
+    }
+
+    private void Update()
+    {
+        m_arr_material[1].mainTextureOffset = new Vector2(1, timer);
+        timer -= speed;
+    }
+}
